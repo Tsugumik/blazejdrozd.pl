de8af3c07850b2ec5ce75f52aee75d729ad5eda0
Błażej Drozd <drozdbazej@gmail.com>
2023-07-30 22:00:26 +0200

    Added transitions

--- src/lib/Card.svelte
+++ src/lib/Card.svelte
@@ -1,3 +1,3 @@
-<div class="bg-gray-800 rounded-lg p-5 m-3 bg-opacity-40 backdrop-blur-xl shadow-xl">
+<div class="bg-gray-800 rounded-lg p-5 m-3 bg-opacity-40 backdrop-blur-xl shadow-xl hover:bg-opacity-80 transition-all">
     <slot/>
 </div>
--- src/lib/SocialLink.svelte
+++ src/lib/SocialLink.svelte
@@ -4,6 +4,6 @@
     export let imgAlt: string;
 </script>
 
-<a href={href} target="_blank" class="bg-gray-300 p-1 rounded-lg w-[35px] h-[35px] flex justify-center items-center">
+<a href={href} target="_blank" class="bg-gray-300 p-1 rounded-lg w-[35px] h-[35px] flex justify-center items-center hover:shadow hover:shadow-gray-50 transition-shadow">
     <img src={imgSrc} alt={imgAlt} width="35px" height="35px">
 </a>
