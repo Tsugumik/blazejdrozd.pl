3ca732bf349cb9564e938c03f6c56a05ca7e0da9
Błażej Drozd <drozdbazej@gmail.com>
2024-07-31 19:36:17 +0200

    migrate

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,7 @@
 
 
 <section class="flex flex-col justify-center items-center gap-5 border-b-2 border-white">
-    <img src="/avatar.jpg" alt="Błażej Drozd" class="rounded-full border-2 border-white" width="200px" height="200px">
+    <img src="/avatar.webp" alt="Błażej Drozd" class="rounded-full border-2 border-white" width="200px" height="200px">
     <div class="font-poppins text-white text-center flex flex-col justify-center items-center gap-2 py-2">
         <h2 class="text-xl">Hello, I'm Błażej Drozd</h2>
         <p class="text-lg">Programmer and computer scientist.</p>
